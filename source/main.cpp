0ceec1d7573d457db61a0f4f788ca1b344151bd2
George Dixon <georgedixon431@gmail.com>
2026-09-21 15:22:31 +1200

    ending when lose as well as checks lateral movement and rotation

--- source/main.cpp
+++ source/main.cpp
@@ -56,6 +56,7 @@ void clearLine(){
             sum += grid[y][x];
         }
         if (sum == 47){ //9 * 5 = 45 for full row + 2 for edges
+            score += 10;
             for (int j = y; j > 0; --j) {
                 grid[j] = grid[j - 1];
             }
@@ -93,6 +94,48 @@ bool canMoveDown()
     return true;
 }
 
+bool canMoveLeft(){
+    if (blockX <= 1){
+        return false;
+    }
+    for (int col = 0; col < 2; col++){
+        if (block[col][0] > 0 && grid[blockY + col][blockX - 1] > 0){
+            return false;
+        }
+    }
+    return true;
+}
+
+bool canMoveRight(){
+    if (blockX >= 4){
+        return false;
+    }
+    for (int col = 0; col < 2; col++){
+        if (block[col][1] > 0 && grid[blockY + col][blockX + 2] > 0){
+            return false;
+        }
+    }
+    return true;
+}
+bool canRotate() {
+    std::array<std::array<int, 2>, 2> rotated = block;
+
+    int spare = rotated[0][0];
+    rotated[0][0] = rotated[0][1];
+    rotated[0][1] = rotated[1][1];
+    rotated[1][1] = rotated[1][0];
+    rotated[1][0] = spare;
+
+    for (int row = 0; row < 2; ++row) {
+        for (int col = 0; col < 2; ++col) {
+            if (rotated[row][col] > 0 && block[row][col] == 0 && grid[blockY + row][blockX + col]) {
+                return false;
+            }
+        }
+    }
+    return true;
+}
+
 void setBlock(){
     if (block[0][0] > 0)
         grid[blockY][blockX] = block[0][0];
@@ -116,19 +159,21 @@ void displaygrid(){
 
 void onButtonAB(MicroBitEvent e)
 {
-    removeBlock();
-    int spare = block[0][0];
-    block[0][0] = block[1][0];
-    block[1][0] = block[1][1];
-    block[1][1] = block[0][1];
-    block[0][1] = spare;
-    setBlock();
-    displaygrid();
+    if (!gameOver && canRotate()){
+        removeBlock();
+        int spare = block[0][0];
+        block[0][0] = block[1][0];
+        block[1][0] = block[1][1];
+        block[1][1] = block[0][1];
+        block[0][1] = spare;
+        setBlock();
+        displaygrid();
+    }
 }
 
 void onButtonA(MicroBitEvent e)
 {
-    if (!gameOver && blockX> 1)
+    if (!gameOver && canMoveLeft())
     {
         removeBlock();
         blockX--;
@@ -139,7 +184,7 @@ void onButtonA(MicroBitEvent e)
 
 void onButtonB(MicroBitEvent e)
 {
-    if (!gameOver && blockX < 4)
+    if (!gameOver && canMoveRight())
     {
         removeBlock();
         blockX++;
@@ -157,6 +202,10 @@ void fallingblocks(){
             blockX = microbit_random(range) + 1;
             shape = microbit_random(blocks);
             block = data[shape];
+            if (!canMoveDown()){
+                gameOver = true;
+                break;
+            }
         } else if(canMoveDown()){
             blockY += dy;
         } else{
@@ -170,6 +219,15 @@ void fallingblocks(){
         uBit.sleep(fallingSpeed);
         removeBlock();
     }
+    uBit.serial.send("Final score: ");
+    uBit.serial.send(ManagedString(score));
+    uBit.serial.send("\r\n");
+
+    uBit.display.image.clear();
+    uBit.display.scroll("LOSER! Score: " + ManagedString(score));
+    
+
+    uBit.reset();
 }
 
 int main()
